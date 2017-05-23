82e3990a23cfdb92aeecf13e25cb82860338502a
Danny Terweij <danny@p-s.nl>
2017-05-24 01:01:48 +0200

    Minor changes
    * Add sensor address search, else it will always fail the first check.
    * Using newer fork of MySQL Connector
    * 10 Bytes remaining before the sketch will crash (Improvement)
    * More serial output.

--- Temperature_Solar/Temperature_Solar.ino
+++ Temperature_Solar/Temperature_Solar.ino
@@ -1,14 +1,18 @@
 #include <OneWire.h>
 #include <Ethernet.h>
-#include <MySQL_Connection.h>
+#include <MySQL_Connection.h> //  https://github.com/copterino/MySQL_Connector_Arduino
 #include <MySQL_Cursor.h>
 #include <Timer.h>
 #include <SevenSegmentTM1637.h>
 // ---------------------------------------------------------------------
 //
 // Sketch may not exceed lower then 825 bytes for local variables, else Arduino uno v3 resets in error
+// Current: Sketch - 28254 bytes used
+// Global variables - 1213 bytes used
+// Local variables - 835 bytes free (10 bytes remaining before it crashes!)
 //
-
+// Ethernet shield
+//
 // Define space for dec2strf function
 char str[16] = "...";
 
@@ -29,15 +33,20 @@ IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 255, 0);
 
 // MySQL Section
-IPAddress server_addr(192, 168, 0, 91);       // IP of the MySQL *server* here
-const char user[] = "temperature";            // mysql user
-const char password[] = "fakepassword";       // mysql password
+IPAddress server_addr(192, 168, 0, 91); // IP of the MySQL *server* here
+const char user[] = "temperatuur";            // mysql user
+const char password[] = "Poema001";           // mysql password
 EthernetClient client;
 MySQL_Connection conn((Client *)&client);
 
 const unsigned int ROOD = 5;        // red led
 const unsigned int GEEL = 6;        // yellow led
 const unsigned int GROEN = 7;       // green led
+
+const unsigned int ld1 = A3;       // led
+const unsigned int ld2 = A4;       // led
+const unsigned int ld3 = A5;       // led
+
 const unsigned long PERIOD1 = 500;  // half second blink for heartbeat led
 const unsigned int solarPin = A0;   // solar at analog pin A0
 
@@ -53,7 +62,7 @@ int wissel = true;                  // change display check
 // ----------------------------------------------------------------------
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println("");
   Serial.println(F("#### START ####"));
 
@@ -61,10 +70,19 @@ void setup() {
   display.setBacklight(100);  // set the brightness to 100 %
   display.clear();
   display.print("INIT");      // display INIT on the display
- 
+
   pinMode(ROOD, OUTPUT);
   pinMode(GEEL, OUTPUT);
   pinMode(GROEN, OUTPUT);
+
+
+  pinMode(ld1, OUTPUT);
+  pinMode(ld2, OUTPUT);
+  pinMode(ld3, OUTPUT);
+
+  digitalWrite(ld1, HIGH);
+  digitalWrite(ld2, HIGH);
+  digitalWrite(ld3, HIGH);
 
   t.oscillate(GROEN, 75, LOW, 2);
   t.oscillate(GEEL, 75, LOW, 2);
@@ -77,7 +95,8 @@ void setup() {
   Serial.print(F("Init IP: "));
 
   Ethernet.begin(mac, ip, myDns, gateway, subnet);
-  
+  delay(500);
+  display.blink();
   teller();
 
   Serial.println(Ethernet.localIP());
@@ -87,18 +106,17 @@ void setup() {
 
   // timers (max = 6)
   int updateEthernetEvent = t.every(30000, updateEthernet, (void*)2); // update ethernet stuff
-  int writeMySQLEvent = t.every(10000, writeMySQL, (void*)2);         // write data to mysql
+  int writeMySQLEvent = t.every(60000, writeMySQL, (void*)2);         // write data to mysql
   int readSensorEvent = t.every(2500, readSensor, (void*)2);          // read temperature
-  int solarEvent = t.every(2500, readSolar, (void*)2);                // read solar
-  int writeDispEvent = t.every(5000, writeDisp, (void*)2);            // write temperature and solar to TM1637 display
+  int solarEvent = t.every(3500, readSolar, (void*)2);                // read solar
+  int writeDispEvent = t.every(6000, writeDisp, (void*)2);            // write temperature and solar to TM1637 display
 
   digitalWrite(ROOD, LOW);
 
   display.print("DONE");
   display.blink();
-  display.setBacklight(25);
+  display.setBacklight(50);
   display.clear();
-
   Serial.println(F("Ready"));
 }
 
@@ -114,8 +132,8 @@ void updateEthernet(void* context)
 
 void readSensor(void* context)
 {
-  Serial.println(F("INFO: Read Temp Sensor"));
-
+  Serial.print(F("INFO: Read Temp Sensor - "));
+  t.oscillate(ld3, 50, LOW, 1);
   byte present = 0;
   byte type_s;
   byte data[12];
@@ -123,13 +141,18 @@ void readSensor(void* context)
 
   byte i;
 
+  ds.reset_search();
+
   if ( !ds.search(addr)) {
     ds.reset_search();
+    t.oscillate(ROOD, 75, LOW, 30);
+    Serial.println(F("SEARCH ERROR"));
     return;
   }
 
   if (OneWire::crc8(addr, 7) != addr[7]) {
     t.oscillate(ROOD, 75, LOW, 30);
+    Serial.println(F("CRC ERROR"));
     return;
   }
 
@@ -140,12 +163,10 @@ void readSensor(void* context)
     default:
       digitalWrite(GEEL, HIGH);
       digitalWrite(ROOD, HIGH);
+      Serial.println(F(" NO SENSOR"));
       return;
   }
 
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44, 1); // reset chip
 
   present = ds.reset();
   ds.select(addr);
@@ -156,16 +177,12 @@ void readSensor(void* context)
   }
 
   int16_t raw = (data[1] << 8) | data[0];
-  if (type_s) {
-    raw = raw << 3;
-    if (data[7] == 0x10) {
-      raw = (raw & 0xFFF0) + 12 - data[6];
-    }
-  }
+  raw = raw << 3;
+  raw = (raw & 0xFFF0) + 12 - data[6];
   celsius = (float)raw / 16.0;
-  disptemp = dec2strf((float)raw / 16.0,1);
-  disptemp.replace(".", "");    
-
+  disptemp = dec2strf((float)raw / 16.0, 1);
+  disptemp.replace(".", "");
+  Serial.println(celsius);
 }
 
 char* dec2strf(float floatNumber, uint8_t dp)
@@ -191,7 +208,6 @@ char* dec2strf(float floatNumber, uint8_t dp)
 
   // For overflow put ... in string and return (Nb. all TFT_ILI9341 library fonts contain . character)
   if (floatNumber >= 2147483647) {
-    //str[ptr++] = '.'; str[ptr++] = '.'; str[ptr++] = '.'; str[ptr] = 0; // 
     strcpy(str, "...");
     return str;
   }
@@ -226,25 +242,24 @@ char* dec2strf(float floatNumber, uint8_t dp)
     ptr++; digits++;         // Increment pointer and digits count
     floatNumber -= temp;     // Remove that digit
   }
-  
+
   // Finally we can return a pointer to the string
   return str;
 }
 
 void writeDisp(void* context)
 {
-
-display.clear();
-display.setColonOn(0);
+  t.oscillate(ld2, 50, LOW, 1);
+  display.clear();
+  display.setColonOn(0);
 
   if (wissel) {
 
     Serial.println(F("INFO: Update display C"));
     display.setColonOn(1);
-    byte rawData;
-    rawData = B01100011;
-    display.print(disptemp);    
-    display.printRaw(rawData,3);
+    byte rawData = B01100011;
+    display.print(disptemp);
+    display.printRaw(rawData, 3);
     wissel = false;
 
   } else {
@@ -267,13 +282,13 @@ display.setColonOn(0);
 
 void writeMySQL(void* context)
 {
-  Serial.println(F("INFO:: Write Temp to MySQL"));
+  Serial.println(F("INFO:      : Write Temp to MySQL"));
 
-  Serial.print(F(" Temp: "));
+  Serial.print(F("# Temp     : "));
   Serial.println(celsius);
-  Serial.print(F(" Solar: "));
-  Serial.print(solarValue);
-  Serial.print(F(" Opbrengst: "));
+  Serial.print(F("# Solar    : "));
+  Serial.println(solarValue);
+  Serial.print(F("# Opbrengst: "));
   Serial.println(solarLevel);
 
   String SQL;
@@ -283,7 +298,7 @@ void writeMySQL(void* context)
 
     MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
     SQL = "UPDATE temperatuur.current SET value='" +  celsius + "', logtime=UNIX_TIMESTAMP(now()) WHERE log_id='3'";
-  
+
     tmp_str_len = SQL.length() + 1;
     char mquery[tmp_str_len];
     SQL.toCharArray(mquery, tmp_str_len);
@@ -317,9 +332,11 @@ void writeMySQL(void* context)
 
 void readSolar(void* context)
 {
-  Serial.println(F("INFO: Read Solar"));
+  t.oscillate(ld1, 50, LOW, 1);
+  Serial.print(F("INFO: Read Solar - "));
   solarValue = analogRead(solarPin);
   solarLevel = map(solarValue, 0, 587, 0, 100);
+  Serial.println(solarLevel);
 }
 
 void soft_reset() {
@@ -331,8 +348,8 @@ void soft_reset() {
 }
 
 void teller() {
- display.clear();
- for ( int c = 20; c >= 0; c--) {
+  display.clear();
+  for ( int c = 20; c >= 0; c--) {
     display.print(c);
     delay(1000);
     display.clear();
