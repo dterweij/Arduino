a4712e5afae283bd50baefd226ba4431d160fcbb
Danny Terweij <danny@p-s.nl>
2017-10-19 22:10:22 +0200

    Server code python.

---
+++ 433Mhz RF sensors/server.py
@@ -0,0 +1,3 @@
+// File comes soon. Server still offline after the move to new house.
+// basicly it reads ttyUSB0 on Ubuntu system and reads info from the Receiver. 
+// The info is then pushed to the database.
